880fd395408690ab19cb86a96da27b51481f35ca
Junhan0037 <wnsgks0037@naver.com>
2020-06-03 22:18:51 +0900

    add Login,Logout method

--- stocklab/agent/ebest.py
+++ stocklab/agent/ebest.py
@@ -1,3 +1,7 @@
+import configparser
+import win32com.client
+import pythoncom
+
 class XASession:
     # 로그인 상태를 확인하기 위한 클래스변수
     login_state = 0
@@ -19,3 +23,39 @@ class XASession:
         """
         print("Session disconnected")
         XASession.login_state = 0
+
+class EBest:
+    
+    def __init__(self, mode=None):
+        """
+        config.ini 파일을 로드해 사용자, 서버 정보 저장
+        query_cnt는 10분당 200개의 TR 수행을 관리하기 위한 리스트
+        xa_session_client는 XASession 객체
+        :param mode:str - 모의서버는 DEMO 실서버는 PROD로 구분
+        """
+        if mode not in ["PROD", "DEMO"]:
+            raise Exception("Need to run_mode(PROD or DEMO)")
+
+        run_mode = "EBEST_" + mode
+        config = configparser.ConfigParser()
+        config.read('conf/config.ini')
+        self.user = config[run_mode]['user']
+        self.passwd = config[run_mode]['password']
+        self.cert_passwd = config[run_mode]['cert_passwd']
+        self.host = config[run_mode]['host']
+        self.port = config[run_mode]['port']
+        self.account = config[run_mode]['account']
+
+        self.xa_session_client = win32com.client.DispatchWithEvents("XA_Session.XASession", XASession)
+
+    def login(self):
+        self.xa_session_client.ConnectServer(self.host, self.port)
+        self.xa_session_client.Login(self.user, self.passwd, self.cert_passwd, 0, 0)
+        while XASession.login_state == 0:
+            pythoncom.PumpWaitingMessages()
+
+    def logout(self):
+        # result = self.xa_session_client.Logout()
+        # if result:
+        XASession.login_state = 0
+        self.xa_session_client.DisconnectServer()
