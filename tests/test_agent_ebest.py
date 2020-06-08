e827a43f8878b306aae9755759bd76a0188f558b
Junhan0037 <wnsgks0037@naver.com>
2020-06-08 21:10:26 +0900

    ADD 주식 주문 API, Test

--- tests/test_agent_ebest.py
+++ tests/test_agent_ebest.py
@@ -47,7 +47,43 @@ class TestEBest(unittest.TestCase):
         result = self.ebest.get_agent_trend_by_code("005930", fromdt="20190304", todt="20190304")
         assert result is not None
         print(result)
+        
+    def test_get_account_info(self):
+        print(inspect.stack()[0][3])
+        result = self.ebest.get_account_info()
+        assert result
+        print(result)
+        
+    def test_get_account_stock_info(self):
+        result = self.ebest.get_account_stock_info()
+        assert result is not None
+        print(result)
+        
+    def test_order_stock(self):
+        print(inspect.stack()[0][3])
+        result = self.ebest.order_stock("005930", "2", "46000", "2", "00")
+        assert result
+        print(result)
+        
+    def test_order_cancel(self):
+        print(inspect.stack()[0][3])
+        result = self.ebest.order_cancel("29515", "A005930", "2")
+        assert result
+        print(result)
+        
+    def test_order_check(self):
+        print(inspect.stack()[0][3])
+        result = self.ebest.order_check("29515")
+        assert result
+        print(result)
+        
+    def test_get_current_call_price_by_code(self):
+        print(inspect.stack()[0][3])
+        result = self.ebest.get_current_call_price_by_code("005930")
+        assert result
+        print(result)
      """
+
 
 
 
